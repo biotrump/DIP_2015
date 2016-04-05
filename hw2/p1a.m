dd55b37f5043a58046d791fc82244f2f259bd86e
Thomas Tsai <thomas@life100.cc>
2016-04-05 17:19:25 +0800

    p1a sobel edge detection

--- hw2/p1a.m
+++ hw2/p1a.m
@@ -5,94 +5,110 @@ row=256;  col=256;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %sobel
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%load image 
-raw_image='raw\sample1.raw';
-fin=fopen(raw_image,'r');
-I=fread(fin,row*col,'uint8=>uint8');
-fclose(fin);
-S1=reshape(I,row,col);
-S1=S1';
+thresholds=[50;100;250];
+for i = 1: size(thresholds,1),
+	threshold = thresholds(i,:);
+	%load image 
+	raw_image='raw\sample1.raw';
+	fin=fopen(raw_image,'r');
+	I=fread(fin,row*col,'uint8=>uint8');
+	fclose(fin);
+	S1=reshape(I,row,col);
+	S1=S1';
 
-%figure,imshow(S1, 'Border','tight');
-threshold=36; 
-threshold=100;
-threshold=150;
-[EM1, BW1] = sobel(S1,threshold, 1);
-figure('name','raw\sample1.raw'),imshowpair(S1,EM1,'montage');
-figure('name','raw\sample1.raw : 01 edge map'),imshowpair(S1,BW1,'montage');
-%%%%%%%%%%%%%%%%%%%%%
-raw_image='raw\sample2.raw';
-fin=fopen(raw_image,'r');
-I=fread(fin,row*col,'uint8=>uint8');
-fclose(fin);
-S2=reshape(I,row,col);
-S2=S2';
+	%figure,imshow(S1, 'Border','tight');
 
-[EM2,BW2] = sobel(S2,threshold,1);
-figure('name','raw\sample2.raw'),imshowpair(S2,EM2,'montage');
-figure('name','raw\sample2.raw : 01 edge map'),imshowpair(S2,BW2,'montage');
-%%%%%%%%%%%%%%%%%%%%%
-raw_image='raw\sample3.raw';
-fin=fopen(raw_image,'r');
-I=fread(fin,row*col,'uint8=>uint8');
-fclose(fin);
-S3=reshape(I,row,col);
-S3=S3';
+	[EM1, BW1] = sobel(S1,threshold, 1);
+	figure('name',raw_image),imshowpair(S1, EM1,'montage');
+	fig_name = sprintf('%s : sobel 01 edge map thr=%d',raw_image, threshold);
+	figure('name',fig_name),imshowpair(S1,BW1,'montage');%title(fig_name);
+	fig_name = sprintf('sample1-sobel-01-edgemap-thre-%d',threshold);
+	print(gcf, '-dpng', fig_name);
 
-[EM3, BW3] = sobel(S3,threshold,1);
-figure('name','raw\sample3.raw'),imshowpair(S3,EM3,'montage');
-figure('name','raw\sample3.raw : 01 edge map'),imshowpair(S3,BW3,'montage');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%show all results
-figure('name','sobel Edge Detection');
-subplot(2,3,1),imshow(S1);
-subplot(2,3,2),imshow(S2);
-subplot(2,3,3),imshow(S3);
-subplot(2,3,4),imshow(BW1);
-subplot(2,3,5),imshow(BW2);
-subplot(2,3,6),imshow(BW3);
+	%%%%%%%%%%%%%%%%%%%%%
+	raw_image='raw\sample2.raw';
+	fin=fopen(raw_image,'r');
+	I=fread(fin,row*col,'uint8=>uint8');
+	fclose(fin);
+	S2=reshape(I,row,col);
+	S2=S2';
+	
+	[EM2,BW2] = sobel(S2,threshold,1);
+	figure('name',raw_image),imshowpair(S2, EM2,'montage');
+	fig_name = sprintf('%s : sobel 01 edge map thr=%d', raw_image, threshold);
+	figure('name',fig_name),imshowpair(S2,BW2,'montage');%title(fig_name);
+	fig_name = sprintf('sample2-sobel-01-edgemap-thre-%d',threshold);
+	print(gcf, '-dpng', fig_name);
+	%%%%%%%%%%%%%%%%%%%%%
+	raw_image='raw\sample3.raw';
+	fin=fopen(raw_image,'r');
+	I=fread(fin,row*col,'uint8=>uint8');
+	fclose(fin);
+	S3=reshape(I,row,col);
+	S3=S3';
+	
+	[EM3, BW3] = sobel(S3,threshold,1);
+	figure('name',raw_image),imshowpair(S3, EM3,'montage');
+	fig_name = sprintf('%s : sobel 01 edge map thr=%d',raw_image, threshold);
+	figure('name',fig_name),imshowpair(S3,BW3,'montage');%title(fig_name);
+	fig_name = sprintf('sample3-sobel-01-edgemap-thre-%d',threshold);
+	print(gcf, '-dpng', fig_name);
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%show all results
+	figure('name','sobel Edge Detection');
+	subplot(2,3,1),imshow(S1);
+	subplot(2,3,2),imshow(S2);
+	subplot(2,3,3),imshow(S3);
+	subplot(2,3,4),imshow(BW1);
+	subplot(2,3,5),imshow(BW2);
+	subplot(2,3,6),imshow(BW3);
+	fig_name = sprintf('sobel-edge-detect-thr-%d',threshold);
+	print(gcf, '-dpng', fig_name);
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %matlab code verification
-method='Sobel';
-direction='both';%'horizontal' | 'vertical'
-
-%figure,imshow(S1, 'Border','tight');
-[BW1,threshOut] = edge(S1,method);
-threshold = threshOut;
-BW1 = edge(S1,method,threshold,direction);
-%figure,imshow(BW1, 'Border','tight');
-figure('name',['matlab +' method]),imshowpair(S1,BW1,'montage');
-
-%
-raw_image='raw\sample2.raw';
-fin=fopen(raw_image,'r');
-I=fread(fin,row*col,'uint8=>uint8');
-fclose(fin);
-S2=reshape(I,row,col);
-S2=S2';
-%figure,imshow(S2, 'Border','tight');
-BW2 = edge(S2,method,threshold,direction);
-%figure,imshow(BW2, 'Border','tight');
-figure('name',['matlab +' method]),imshowpair(S2,BW2,'montage');
-
-%%%
-raw_image='raw\sample3.raw';
-fin=fopen(raw_image,'r');
-I=fread(fin,row*col,'uint8=>uint8');
-fclose(fin);
-S3=reshape(I,row,col);
-S3=S3';
-%figure,imshow(S3, 'Border','tight');
-BW3 = edge(S3,method,threshold,direction);
-%figure,imshow(BW3, 'Border','tight');
-figure('name',['matlab +' method]),imshowpair(S3,BW3,'montage');
-%
-%show all results
-figure('name','matlab Sobel Edge Detection');
-subplot(2,3,1),imshow(S1);
-subplot(2,3,2),imshow(S2);
-subplot(2,3,3),imshow(S3);
-subplot(2,3,4),imshow(BW1);
-subplot(2,3,5),imshow(BW2);
-subplot(2,3,6),imshow(BW3);
+if 0,
+	method='Sobel';
+	direction='both';%'horizontal' | 'vertical'
+	
+	%figure,imshow(S1, 'Border','tight');
+	[BW1,threshOut] = edge(S1,method);
+	threshold = threshOut;
+	BW1 = edge(S1,method,threshold,direction);
+	%figure,imshow(BW1, 'Border','tight');
+	figure('name',['matlab +' method]),imshowpair(S1,BW1,'montage');
+	
+	%
+	raw_image='raw\sample2.raw';
+	fin=fopen(raw_image,'r');
+	I=fread(fin,row*col,'uint8=>uint8');
+	fclose(fin);
+	S2=reshape(I,row,col);
+	S2=S2';
+	%figure,imshow(S2, 'Border','tight');
+	BW2 = edge(S2,method,threshold,direction);
+	%figure,imshow(BW2, 'Border','tight');
+	figure('name',['matlab +' method]),imshowpair(S2,BW2,'montage');
+	
+	%%%
+	raw_image='raw\sample3.raw';
+	fin=fopen(raw_image,'r');
+	I=fread(fin,row*col,'uint8=>uint8');
+	fclose(fin);
+	S3=reshape(I,row,col);
+	S3=S3';
+	%figure,imshow(S3, 'Border','tight');
+	BW3 = edge(S3,method,threshold,direction);
+	%figure,imshow(BW3, 'Border','tight');
+	figure('name',['matlab +' method]),imshowpair(S3,BW3,'montage');
+	%
+	%show all results
+	figure('name','matlab Sobel Edge Detection');
+	subplot(2,3,1),imshow(S1);
+	subplot(2,3,2),imshow(S2);
+	subplot(2,3,3),imshow(S3);
+	subplot(2,3,4),imshow(BW1);
+	subplot(2,3,5),imshow(BW2);
+	subplot(2,3,6),imshow(BW3);
+end
--- hw2/sobel.m
+++ hw2/sobel.m
@@ -3,6 +3,11 @@
 % @PARAM image uint8 grey image
 % @PARAM Thresh=100
 function [EM, BEM]=sobel(image, Thresh, show)
+	persistent my_count;
+    if isempty(my_count)
+        my_count=1;
+    end
+    
 	if nargin < 3
 	    show=0;
 	end
@@ -16,17 +21,19 @@ function [EM, BEM]=sobel(image, Thresh, show)
 
 	for i=1:size(I,1)-2
 		for j=1:size(I,2)-2
-			%Gx : Sobel mask for x-direction:
-			dx=((2*I(i+2,j+1)+I(i+2,j)+I(i+2,j+2))-(2*I(i,j+1)+I(i,j)+I(i,j+2)));
-			%Gy : Sobel mask for y-direction:
-			dy=((2*I(i+1,j+2)+I(i,j+2)+I(i+2,j+2))-(2*I(i+1,j)+I(i,j)+I(i+2,j)));
+			%Gx : Sobel mask for x-direction: Column gradient
+			dx=( (I(i+2,j) + 2*I(i+2,j+1) + I(i+2,j+2) ) - ( I(i,j) + 2*I(i,j+1)+ I(i,j+2) ) );
+			%Gy : Sobel mask for y-direction: row gradient
+			dy=( ( I(i,j+2) + 2*I(i+1,j+2) + I(i+2,j+2) )- ( I(i,j) + 2*I(i+1,j) + I(i+2,j) ) );
 			%Orthogonal gradient Magnitude
 			G(i,j)=sqrt(dx.^2+dy.^2);
       Gx(i,j)=dx;
       Gy(i,j)=dy;
 		end
-    end
-    %h = histogram(G);
+	end
+	%
+    %subplot(2,2,2);  h = histogram(G,20, 'BinMethod','sturges');
+    %subplot(2,2,2); set(gca,'xscale','log') ; h = hist(G);
 	if show,
 		G_Nx = abs(Gx);	
 		G_Nx = normalize(G_Nx);
@@ -36,24 +43,30 @@ function [EM, BEM]=sobel(image, Thresh, show)
 		subplot(2,2,3); imshow(G_Ny); title('Sobel gradient:Gy');
 		G_N = normalize(G);
 		subplot(2,2,4); imshow(G_N); title('Sobel gradient: G=Gx^2+Gy^2');
-    end
-    %threshold the magnitude: if the magnitude <= threshold. set it to "0"
-    %else keep the magnitude.
-    % max(G, Thresh) : if G(i,j) <= threshold, G(i,j)= threshold,
-    % else if G(i,j) > threshold, G(i,j) is kept intact.
+		fig_name = sprintf('G-sample%d-sobel-edge-detect-thr-%d',my_count, Thresh);
+		print(gcf, '-dpng', fig_name);		
+	end
+	%threshold the magnitude: if the magnitude <= threshold. set it to "0"
+	%else keep the magnitude.
+	% max(G, Thresh) : if G(i,j) <= threshold, G(i,j)= threshold,
+	% else if G(i,j) > threshold, G(i,j) is kept intact.
 	EM=max(G,Thresh);
-    %compare each element of EM to round(Thresh), if the element is equal
-    %to round(Thresh), it's 1 else it's 0.
-    %So if the elements of EM == round(threshold) will become "0".
-    %else the elements of EM does not change.
+	%compare each element of EM to round(Thresh), if the element is equal
+	%to round(Thresh), it's 1 else it's 0.
+	%So if the elements of EM == round(threshold) will become "0".
+	%else the elements of EM does not change.
 	EM(EM==round(Thresh))=0;% elements <= threshold becomes "0"
 	EM=uint8(EM);
 
-    %binary edge map, 1: edge, 0 : background
-    BEM= G >= Thresh;
+	%binary edge map, 1: edge, 0 : background
+	BEM= G >= Thresh;
 	if show,	
 		%subplot(2,2,3);	
-        %edge intensity "1", background is "0"
-        %figure();imshow(EM);title('Sobel Edge detected Image');
+	    %edge intensity "1", background is "0"
+	    %figure();imshow(EM);title('Sobel Edge detected Image');
 	end
+	figure('name', ['hist ' fig_name]);
+    h = histogram(G,50, 'BinMethod','sturges');
+    print(gcf, '-dpng', fig_name);
+	my_count = my_count +1;
 end
